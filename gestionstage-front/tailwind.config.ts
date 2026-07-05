6fb7cc9add0408b656cfc17255931c3f8479425e
khadijaabirat <khadijaabirat@gmail.com>
2026-07-05 23:27:29 +0100

    chore(frontend): update Tailwind config with custom theme colors and plugins

--- gestionstage-front/tailwind.config.ts
+++ gestionstage-front/tailwind.config.ts
@@ -1,6 +1,10 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  darkMode: 'class',
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
