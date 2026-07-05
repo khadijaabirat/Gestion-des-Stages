6523567a0ffe52754e624ca201eb15e86ae6d571
khadijaabirat <khadijaabirat@gmail.com>
2026-07-05 23:22:12 +0100

    feat(public): add public company info and directory page

---
+++ gestionstage-front/app/entreprises-info/page.tsx
@@ -0,0 +1,9 @@
+import EntreprisesInfoContent from '@/components/entreprises-info/EntreprisesInfoContent';
+
+export default function EntreprisesInfoPage() {
+  return (
+    <div className="pt-0 min-h-screen">
+      <EntreprisesInfoContent />
+    </div>
+  );
+}
